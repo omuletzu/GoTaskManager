49efe490b6651763f036716c52269016ee5964f2
omuletzu <mihneasimota1234@gmail.com>
2025-11-09 12:53:25 +0200

    added env for apollo

--- src/app/api/ApolloClient.tsx
+++ src/app/api/ApolloClient.tsx
@@ -2,10 +2,10 @@ import { HttpLink } from "@apollo/client";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const apolloClient = new ApolloClient({
-    link: new HttpLink({
-        uri: "http://localhost:8080/graphql"
-    }),
-    cache: new InMemoryCache()
-})
+  link: new HttpLink({
+    uri: process.env.NEXT_PUBLIC_GQL_URL,
+  }),
+  cache: new InMemoryCache(),
+});
 
-export default apolloClient
+export default apolloClient;
